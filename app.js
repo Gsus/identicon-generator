c8589e0b570c8e78cf6acff108ad5ee4c45f3a6c
Gsus_asb <usjasb@gmail.com>
2020-12-10 13:20:02 -0400

    Basic functionality and styling

---
+++ app.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: "#root",
+  data: {
+    identiconText: "",
+  },
+  computed: {
+    identicon() {
+      return jdenticon.toSvg(this.identiconText, 200);
+    },
+  },
+  methods: {
+    onInput(event) {
+      this.identiconText = event.target.value;
+      console.log("running");
+    },
+  },
+});
